96a9cd7cf453fdf357177a78ccd985c817d8a81d
KoyanagiAyuha <ayuha.koyanagi@gmail.com>
2022-05-13 23:34:56 +0900

    testcommit

--- mosaic-resultimage-lambda/lambda_function.py
+++ mosaic-resultimage-lambda/lambda_function.py
@@ -1,10 +1,47 @@
-import json
-import boto3
 import os
-import base64
-from datetime import datetime, timedelta, timezone
-from mimetypes import guess_extension
-import uuid
+import boto3
+from boto3.dynamodb.conditions import Key
+import logging
+import sys
+from typing import Union
+import traceback
+import json
+
+# 既存ロガーハンドラーの削除
+default_logger = logging.getLogger()
+for h in default_logger.handlers:
+    default_logger.removeHandler(h)
+logger = logging.getLogger(__name__)
+# ルートロガー設定
+logger.setLevel(logging.DEBUG)
+# コンソール出力用のハンドラー
+to_stream = logging.StreamHandler(stream=sys.stdout)
+# ハンドラーごとの出力レベル定義
+to_stream.setLevel(logging.DEBUG)
+# 引数:fmtにformatterを渡す＆時刻の書式を好みの形になるように引数:datefmtへ
+log_format = logging.Formatter("%(levelname)s - %(filename)s - %(funcName)s - %(message)s")
+# それぞれのハンドラへformatterをセットしてやる
+to_stream.setFormatter(log_format)
+# ロガーへハンドラーを追加する
+logger.addHandler(to_stream)
+
+
+def log_decorator():
+    def _log_decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                logger.info('{}関数の処理を開始します'.format(func.__name__))
+                return func(*args, **kwargs)
+
+            except Exception as e:
+                raise e
+
+            finally:
+                logger.info('{}関数の処理を終了します'.format(func.__name__))
+
+        return wrapper
+
+    return _log_decorator
 
 
 def put_posts(title, username, imglist, created_on, postid):
