278b5466c6d55439010fe024e3797d1a49475dec
KoyanagiAyuha <ayuha.koyanagi@gmail.com>
2022-05-29 22:43:43 +0900

    add:モザイク機能の追加

--- mosaic-mosaic-lambda/lambda_function.py
+++ mosaic-mosaic-lambda/lambda_function.py
@@ -1,6 +1,7 @@
 import uuid
 import boto3
 from boto3.dynamodb.conditions import Key
+from botocore.config import Config
 import logging
 import sys
 import json
@@ -8,6 +9,13 @@ from datetime import datetime, timedelta, timezone
 import base64
 import cv2
 import numpy as np
+
+config = Config(
+    retries={
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
 
 # 既存ロガーハンドラーの削除
 default_logger = logging.getLogger()
@@ -32,6 +40,9 @@ EDIT_TABLE_NAME = "mosaic-dev-editpicture-table"
 EDIT_BUCKET_NAME = 'mosaic-dev-resultimg-597775291172'
 SUBJECT_BUCKET_NAME = 'mosaic-dev-registerimg-597775291172'
 SUBJECT_TABLE_NAME = 'mosaic-dev-registerpic-table'
+
+CONTINUE_NUM = 5
+MATCH_TH = 0.8
 
 
 def log_decorator():
@@ -80,7 +91,7 @@ def check_limit(username, now):
         KeyConditionExpression=Key('username').eq(username) & Key('created_at').begins_with(now_format)
     )
 
-    if len(response['Items']) < 5:
+    if len(response['Items']) < CONTINUE_NUM:
         return True
     else:
         return False
@@ -105,10 +116,129 @@ def create_record(username, created_at, post_id, img_title):
 
 
 @log_decorator()
-def create_mosaic_img(img):
+def create_mosaic_img(img, username):
     cv_img = base64_to_cv2(img)
-    base64_img = cv2_to_base64(cv_img)
+
+    # amazon_rekognition に投げる
+    client = boto3.client('rekognition', config=config)
+    result, buf = cv2.imencode('.jpg', cv_img)
+
+    faces = client.detect_faces(Image={'Bytes': buf.tobytes()}, Attributes=['ALL'])
+
+    h, w, ch = cv_img.shape
+    result_im = cv_img.copy()
+
+    subject_img_list = get_subject_img(username)
+
+    match_list = []
+    for subject_img in subject_img_list:
+        img_name = subject_img['uuid']
+        key = '{}/{}.jpg'.format(username, img_name)
+        is_get_img, sub_img = get_image(key)
+        if not is_get_img:
+            continue
+        sub_img = base64_to_cv2(sub_img)
+        result0, buf0 = cv2.imencode('.jpg', sub_img)
+        try:
+            cfaces = client.compare_faces(SourceImage={'Bytes': buf0.tobytes()}, TargetImage={'Bytes': buf.tobytes()})
+        except Exception:
+            continue
+        match_list.extend([i['Face']['BoundingBox'] for i in cfaces['FaceMatches'] if i['Similarity'] > MATCH_TH])
+
+    for i in faces['FaceDetails']:
+        top = max(int(i['BoundingBox']['Top'] * h), 0)
+        left = max(int(i['BoundingBox']['Left'] * w), 0)
+        bottom = min(int(top + i['BoundingBox']['Height'] * h), h)
+        right = min(int(left + i['BoundingBox']['Width'] * w), w)
+
+        is_match = True
+        for j in match_list:
+            match_top = max(int(j['Top'] * h), 0)
+            match_left = max(int(j['Left'] * w), 0)
+            match_bottom = min(int(top + j['Height'] * h), h)
+            match_right = min(int(left + j['Width'] * w), w)
+
+            iou_val = iou((left, top, right, bottom), (match_left, match_top, match_right, match_bottom))
+            if iou_val > 0.8:
+                is_match = False
+
+        if is_match:
+            result_im[top:bottom, left:right] = _mosaic(cv_img[top:bottom, left:right])
+
+    for j in match_list:
+        match_top = max(int(j['Top'] * h), 0)
+        match_left = max(int(j['Left'] * w), 0)
+        match_bottom = min(int(top + j['Height'] * h), h)
+        match_right = min(int(left + j['Width'] * w), w)
+
+        result_im[match_top:match_bottom, match_left:match_right] = cv_img[match_top:match_bottom, match_left:match_right]
+
+    base64_img = cv2_to_base64(result_im)
     return base64_img
+
+
+@log_decorator()
+def get_subject_img(username):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(SUBJECT_TABLE_NAME)
+
+    response = table.query(KeyConditionExpression=Key('username').eq(username))
+
+    return response['Items']
+
+
+@log_decorator()
+def get_image(key):
+
+    s3 = boto3.client('s3')
+
+    try:
+        body = s3.get_object(Bucket=SUBJECT_BUCKET_NAME, Key=key)['Body'].read()
+    except Exception:
+        return False, ''
+
+    encode = base64.b64encode(body).decode()
+
+    return True, encode
+
+
+@log_decorator()
+def _mosaic(src: any, ratio=0.14) -> any:
+    """モザイク変換
+
+    Parameters
+    ----------
+    src : any
+        画像
+    ratio : float, optional
+        モザイク率, by default 0.14
+
+    Returns
+    -------
+    any
+        モザイク変換画像
+    """
+    small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+    return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+
+
+@log_decorator()
+def iou(a: tuple, b: tuple) -> float:
+    a_x1, a_y1, a_x2, a_y2 = a
+    b_x1, b_y1, b_x2, b_y2 = b
+
+    if a == b:
+        return 1.0
+    elif (
+        (a_x1 <= b_x1 and a_x2 > b_x1) or (a_x1 >= b_x1 and b_x2 > a_x1)
+    ) and (
+        (a_y1 <= b_y1 and a_y2 > b_y1) or (a_y1 >= b_y1 and b_y2 > a_y1)
+    ):
+        intersection = (min(a_x2, b_x2) - max(a_x1, b_x1)) * (min(a_y2, b_y2) - max(a_y1, b_y1))
+        union = (a_x2 - a_x1) * (a_y2 - a_y1) + (b_x2 - b_x1) * (b_y2 - b_y1) - intersection
+        return intersection / union
+    else:
+        return 0.0
 
 
 def post_image(img, key):
